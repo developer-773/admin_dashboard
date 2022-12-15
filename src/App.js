ec70aeeb28101ae488566d14028dd23621d50a15
developer-773 <galaxylogin@yandex.com>
2022-12-15 11:59:32 +0500

    Next level

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+     {/* Sidebar */}
+     <div className='main'>
+       {/* Header */}
+       {/* Statistics */}
+       {/* Diagram */}
+      {/* Footer */}
+     </div>
     </div>
   );
 }
